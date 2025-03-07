ccfeba244fe3b9f22ca67e8cebf3303cecd96a4c
sierrapablo <pablosierralorente@outlook.es>
2025-03-07 18:16:14 +0100

    ⬆️ major: Add first hero section

---
+++ src/sections/hero/Hero.jsx
@@ -0,0 +1,13 @@
+import './Hero.css'
+
+function Hero() {
+  return (
+    <section className='heroContainer' id='hero'>
+      <div className='heroOverlay'></div>
+      <h1 className='heroTitle name'>Pablo Sierra</h1>
+      <h1 className='heroTitle description'>Software Engineer</h1>
+    </section>
+  )
+}
+
+export default Hero
