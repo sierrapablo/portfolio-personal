bdd35947084e6f4748697aed4a124d4798120f81
sierrapablo <pablosierralorente@outlook.es>
2025-03-11 14:14:06 +0100

    🔧 improvement: remove touch gestures for scrolling in tactile devices

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Contact from "./sections/contact/Contact"
 
 function App() {
   const [visibleSection, setVisibleSection] = useState('hero')
-  const [touchStartY, setTouchStartY] = useState(0)
 
   const handleWheel = (event) => {
     if (event.deltaY > 0) {
@@ -23,46 +22,15 @@ function App() {
     }
   }
 
-  const handleTouchStart = (event) => {
-    setTouchStartY(event.touches[0].clientY)
-  }
-
-  const handleTouchMove = (event) => {
-    const touchEndY = event.touches[0].clientY
-    const deltaY = touchStartY - touchEndY
-
-    if (Math.abs(deltaY) > 50) {
-      if (deltaY > 0) {
-        setVisibleSection((prevSection) => {
-          if (prevSection === 'hero') return 'about'
-          if (prevSection === 'about') return 'contact'
-          return prevSection
-        })
-      } else {
-        setVisibleSection((prevSection) => {
-          if (prevSection === 'contact') return 'about'
-          if (prevSection === 'about') return 'hero'
-          return prevSection
-        })
-      }
-      setTouchStartY(touchEndY)
-    }
-  }
-
   useEffect(() => {
     const appContainer = document.querySelector('.appContainer')
 
     appContainer.addEventListener('wheel', handleWheel)
 
-    appContainer.addEventListener('touchstart', handleTouchStart)
-    appContainer.addEventListener('touchmove', handleTouchMove)
-
     return () => {
       appContainer.removeEventListener('wheel', handleWheel)
-      appContainer.removeEventListener('touchstart', handleTouchStart)
-      appContainer.removeEventListener('touchmove', handleTouchMove)
     }
-  }, [touchStartY])
+  })
 
   return (
     <div className="appContainer">
